f02b393d9992f15a8f06465b91acc9a4cb1665f5
pengli <justdoit.pli@gmail.com>
2020-12-30 08:59:25 +0800

    add logic

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,21 +3,19 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"os"
-	"strings"
+	"strconv"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 	v1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	tektoncdclientset "github.com/tektoncd/pipeline/pkg/client/clientset/versioned"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
-	"knative.dev/pkg/apis"
 )
 
 const (
-	//PREFIX help to decide if the task is a job activity one
-	PREFIX           = "job-activity-"
+	//TektonTTL set by user express the TTL with seconds
 	TektonTTL string = "tekton.dev/ttl"
 )
 
@@ -30,11 +28,10 @@ func init() {
 }
 
 func main() {
+	log.Info("Start to clean the Pipelinerun based on the TTL.")
 	var namespace string
-	var name string
 
 	flag.StringVar(&namespace, "namespace", "", "The namespace of the pipelinerun.")
-	flag.StringVar(&name, "name", "", "The name of the pipelinerun.")
 	flag.Parse()
 
 	config, err := rest.InClusterConfig()
@@ -53,35 +50,35 @@ func main() {
 		LabelSelector: TektonTTL,
 	}
 
-	pr, err := tektonClient.TektonV1beta1().PipelineRuns(namespace).List(context.TODO(), name, listOptions)
+	prs, err := tektonClient.TektonV1beta1().PipelineRuns(namespace).List(context.TODO(), listOptions)
 	if err != nil {
-		fmt.Printf("Get Pipelinerun: %s failed: %+v", namespace+"/"+name, err)
+		log.Errorf("List Pipelinerun failed: %+v", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(pr.Name)
-
-	/*
-		loop the Pipelinerun.Status.TaskRuns
-		1. If the Pipelinerun.Status.TaskRuns[x].PipelineTaskName not prefix as "job-activity", ignore, that's means the task is not a Job Avtivity.
-		2. Pipelinerun.Status.TaskRuns[x].Status.Conditions[0].type == Succeeded and Pipelinerun.Status.TaskRuns[x].Status.Conditions[0].status == True, means task success.
-	*/
-	results := []exceptionResult{}
-	for _, tr := range pr.Status.TaskRuns {
-		if !strings.HasPrefix(tr.PipelineTaskName, PREFIX) {
+	for _, pr := range prs.Items {
+		ttl, err := strconv.ParseInt(pr.ObjectMeta.Labels[TektonTTL], 10, 64)
+		if err != nil {
+			log.Errorf("Value of ttl is not correct: %+v", err)
 			continue
 		}
 
-		if tr.Status.GetCondition(apis.ConditionSucceeded).IsUnknown() {
-			//the task failed, need write the failed reason to result
-			result := exceptionResult{}
-			result.TaskrunName = tr.PipelineTaskName
-			result.Result = tr.Status.TaskRunResults
-			results = append(results, result)
+		if pr.IsDone() || pr.IsCancelled() {
+			startTime := pr.Status.CompletionTime
+			existTime := time.Since(startTime.Time)
+
+			if existTime.Seconds() > float64(ttl) {
+				err = tektonClient.TektonV1beta1().PipelineRuns(namespace).Delete(context.TODO(), pr.Name, metav1.DeleteOptions{})
+				if err != nil {
+					log.Errorf("Delete pr: %s failed: %+v", pr.Name, err)
+					continue
+				}
+			}
+
 		}
+
 	}
+
+	log.Info("Clean Pipelinerun complete.")
+	os.Exit(0)
 }
-
-// func saveResult() error {
-
-// }
